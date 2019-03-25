c33fec2b515cd97af74803d6ef58d03a0b1faaca
Rick de Vries <rick.de.vries@hva.nl>
2019-03-25 12:54:14 +0100

    Fixed RecyclerView
    RecyclerView is fixed

--- app/src/main/java/com/example/geoguessswipe/MainActivity.java
+++ app/src/main/java/com/example/geoguessswipe/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
@@ -34,13 +35,19 @@ public class MainActivity extends AppCompatActivity implements RecyclerView.OnIt
                     GeoObject.PRE_DEFINED_GEO_OBJECT_IMAGE_IDS[i]));
         }
 
-        final RecyclerView mGeoRecyclerView = findViewById(R.id.recyclerView);
+        GeoObjectAdapter mAdapter = new GeoObjectAdapter(this, mGeoObjects);
+        RecyclerView mGeoRecyclerView = findViewById(R.id.recyclerView);
+        mGeoRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        mGeoRecyclerView.setAdapter(mAdapter);
+        mGeoRecyclerView.addItemDecoration(new DividerItemDecoration(getApplicationContext(), DividerItemDecoration.VERTICAL));
+
+        /*final RecyclerView mGeoRecyclerView = findViewById(R.id.recyclerView);
 
         RecyclerView.LayoutManager mLayoutManager = new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL);
         mGeoRecyclerView.setLayoutManager(mLayoutManager);
         mGeoRecyclerView.setHasFixedSize(true);
         GeoObjectAdapter mAdapter = new GeoObjectAdapter(this, mGeoObjects);
-        mGeoRecyclerView.setAdapter(mAdapter);
+        mGeoRecyclerView.setAdapter(mAdapter);*/
         mGeoRecyclerView.addOnItemTouchListener(this);
 
         mGestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
